233fd271e1c4288c4dd92b73ee55ccdb753a0609
230Dersler <vektorel002@gmail.com>
2024-02-29 19:25:33 +0300

    sınıf ekelendi

---
+++ 29022024_GitHub/Ogrenci.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _29022024_GitHub
+{
+    public class Ogrenci
+    {
+        public int OgrenciID { get; set; }
+    }
+}
