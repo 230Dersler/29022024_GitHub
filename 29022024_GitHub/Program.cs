ce7f2f817fd1544fa580d158fdf4047f590c581c
230Dersler <vektorel002@gmail.com>
2024-02-29 19:15:08 +0300

    Add project files.

---
+++ 29022024_GitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _29022024_GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
